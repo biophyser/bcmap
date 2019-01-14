a67e218e192fd853b18b01ca847be60964e98ea4
Michael J. Harms <harmsm@gmail.com>
2019-01-14 11:29:52 -0800

    going to attempt real production

--- bcmap/experiment.py
+++ bcmap/experiment.py
@@ -110,13 +110,140 @@ class Experiment:
 
         return consensus, support, num_mismatch
 
-    def map_from_cluster_files(self,cluster_directory,key_size=25):
+    def _run_batch(self,batch):
+        """
+        Call the genotypes for each of the clusters in batch.
+        """
 
-        cluster_files = os.listdir(cluster_directory)
-        cluster_files.sort()
-        out_root = os.path.split(cluster_directory)[-1]
-        if out_root == "":
-            out_root = os.path.split(cluster_directory)[0]
+        cons_buffer = []
+        prot_buffer = []
+        clust_buffer = []
+
+        for b in batch:
+            cons, prot, clust = self._call_genotype(b)
+            if cons is not None:
+                cons_buffer.append(cons)
+                prot_buffer.append(prot)
+                clust_buffer.append(clust)
+
+        return cons_buffer, prot_buffer, clust_buffer
+
+
+    def _call_genotype(self,cf):
+        """
+        Call the genotype of a given cluster. Store the outputs in the
+        appropriate buffers.
+        """
+
+        # cluster number
+        cluster_number = cf.split("_")[1].split(".")[0]
+
+        n_term_reads = []
+        c_term_reads = []
+        unique_bc = {}
+        with open(os.path.join(self._cluster_directory,cf.strip())) as f:
+            for l in f:
+                col = l.split("|")
+
+                bc = col[0][:self._key_size].strip()
+                bases = col[2].strip()
+                quals = np.array([data.Q_DICT[k] for k in list(col[3].strip())])
+
+                # HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
+                # Hacking to deal with issues with N- and C-terminal
+                # read assignment and truncation.  For production, this
+                # should go into process_reads.
+                if bases[-9:] == "GCCTAATAA":
+                    c_term_reads.append((bases,quals))
+                else:
+                    if len(bases) > 346:
+                        bases = bases[:346]
+                        quals = quals[:346]
+
+                    # Trim off any extra TAA
+                    while bases[-3:] == "TAA":
+                        bases = bases[:-3]
+                        quals = quals[:-3]
+
+                    n_term_reads.append((bases,quals))
+                # HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
+
+                unique_bc[bc] = cluster_number
+
+        num_n = len(n_term_reads)
+        num_c = len(c_term_reads)
+
+        # Make sure that both N- and C-terminal sequences are seen
+        if num_n == 0 or num_c == 0:
+            return None, None, None
+
+        # somewhat cryptic call splits:
+        # reads = [(s1,q1),(s2,q2),...(sn,qn)] into
+        # (s1,s2,...,sn), (q1,q2,...,qn)
+        n_bases, n_quals = zip(*n_term_reads)
+        c_bases, c_quals = zip(*c_term_reads)
+
+        # Get N- and C-terminal sequences for this cluster
+        n_term_probs = self._ref.calc_base_prob(n_bases,n_quals,align_three_prime=False)
+        c_term_probs = self._ref.calc_base_prob(c_bases,c_quals,align_three_prime=True)
+
+        # Get the consensus sequence defined by these
+        consensus, support, num_mismatch = self._call_consensus(n_term_probs,
+                                                                c_term_probs)
+        consensus = "".join(consensus)
+
+        #Create human-readable support
+        support_to_write = []
+        for s in support:
+            v = "{:.1f}".format(np.round(s))
+            if v == "1.0": v = "0.9"
+            support_to_write.append(v[-1])
+        support_to_write = "".join(support_to_write)
+
+        protein = self._ref.translate(consensus)
+        protein = "".join(protein)
+
+        allele = self._ref.get_allele(consensus)
+        allele = "".join(allele)
+
+        # Write outputs
+        cons = []
+        cons.append(">cluster_{},{},{},{},{}\n".format(cluster_number,
+                                                       allele,
+                                                       num_n,
+                                                       num_c,
+                                                       num_mismatch))
+        cons.append("{}\n".format(consensus))
+        cons.append("{}\n".format(support_to_write))
+        cons.append("{}\n".format(util.prob_to_seq(n_term_probs)))
+        cons.append("{}\n".format(util.prob_to_seq(c_term_probs)))
+
+        prot = []
+        prot.append(">cluster_{},{},{},{},{}\n".format(cluster_number,
+                                                       allele,
+                                                       num_n,
+                                                       num_c,
+                                                       num_mismatch))
+        prot.append("{}\n".format(protein))
+
+        # Write out cluster numbers
+        clust = []
+        for bc in unique_bc.keys():
+            # Write cluster/barcode pairs to a file
+            clust.append("{},{},{}\n".format(bc,cluster_number,allele))
+
+        return "".join(cons), "".join(prot), "".join(clust)
+
+
+    def map_from_cluster_files(self,list_of_cluster_files,cluster_directory,
+                               out_root,key_size=25,batch_size=100):
+
+        self._processes = []
+
+        self._list_of_cluster_files = list_of_cluster_files
+        self._cluster_directory = cluster_directory
+        self._out_root = out_root
+        self._key_size = key_size
 
         # Make output file names
         cluster_out_file = "{}.clusters".format(out_root)
@@ -135,114 +262,45 @@ class Experiment:
         consensus_out = open(consensus_out_file,"w")
         prot_out = open(prot_out_file,"w")
 
-        for i, cf in enumerate(cluster_files):
+        self._cluster_buffer = []
+        self._consensus_buffer = []
+        self._prot_buffer = []
 
-            # cluster number
-            cluster_number = cf.split("_")[1].split(".")[0]
+        i = 0
+        batch_queue = []
+        with open(list_of_cluster_files) as list_pointer:
+            for cf in list_pointer:
 
-            n_term_reads = []
-            c_term_reads = []
-            unique_bc = {}
-            with open(os.path.join(cluster_directory,cf)) as f:
-                for l in f:
-                    col = l.split("|")
+                if len(batch_queue) < batch_size:
+                    batch_queue.append(cf)
+                else:
 
-                    bc = col[0][:key_size].strip()
-                    bases = col[2].strip()
-                    quals = np.array([data.Q_DICT[k] for k in list(col[3].strip())])
+                    # Dump this call onto a thread
+                    cons, prot, clust = self._run_batch(batch_queue)
 
-                    # HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
-                    # Hacking to deal with issues with N- and C-terminal
-                    # read assignment and truncation.  For production, this
-                    # should go into process_reads.
-                    if bases[-9:] == "GCCTAATAA":
-                        c_term_reads.append((bases,quals))
-                    else:
-                        if len(bases) > 346:
-                            bases = bases[:346]
-                            quals = quals[:346]
+                    # Do this on main thread
+                    consensus_out.write("".join(cons))
+                    prot_out.write("".join(prot))
+                    cluster_out.write("".join(clust))
 
-                        # Trim off any extra TAA
-                        while bases[-3:] == "TAA":
-                            bases = bases[:-3]
-                            quals = quals[:-3]
+                    batch_queue = []
 
-                        n_term_reads.append((bases,quals))
-                    # HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
+                # Flush output files
+                if i % batch_size == 0:
 
-                    unique_bc[bc] = cluster_number
+                    cluster_out.flush()
+                    consensus_out.flush()
+                    prot_out.flush()
+                    print(i)
+                    sys.stdout.flush()
 
-            num_n = len(n_term_reads)
-            num_c = len(c_term_reads)
+                i += 1
 
-            # Make sure that both N- and C-terminal sequences are seen
-            if num_n == 0 or num_c == 0:
-                continue
-
-            # somewhat cryptic call splits:
-            # reads = [(s1,q1),(s2,q2),...(sn,qn)] into
-            # (s1,s2,...,sn), (q1,q2,...,qn)
-            n_bases, n_quals = zip(*n_term_reads)
-            c_bases, c_quals = zip(*c_term_reads)
-
-            # Get N- and C-terminal sequences for this cluster
-            n_term_probs = self._ref.calc_base_prob(n_bases,n_quals,align_three_prime=False)
-            c_term_probs = self._ref.calc_base_prob(c_bases,c_quals,align_three_prime=True)
-
-            # Get the consensus sequence defined by these
-            consensus, support, num_mismatch = self._call_consensus(n_term_probs,
-                                                                    c_term_probs)
-            consensus = "".join(consensus)
-
-            #Create human-readable support
-            support_to_write = []
-            for s in support:
-                v = "{:.1f}".format(np.round(s))
-                if v == "1.0": v = "0.9"
-                support_to_write.append(v[-1])
-            support_to_write = "".join(support_to_write)
-
-            protein = self._ref.translate(consensus)
-            protein = "".join(protein)
-
-            allele = self._ref.get_allele(consensus)
-            allele = "".join(allele)
-
-
-            # Write outputs
-            consensus_out.write(">cluster_{},{},{},{},{}\n".format(cluster_number,
-                                                                   allele,
-                                                                   num_n,
-                                                                   num_c,
-                                                                   num_mismatch))
-            consensus_out.write("{}\n".format(consensus))
-            consensus_out.write("{}\n".format(support_to_write))
-            consensus_out.write("{}\n".format(util.prob_to_seq(n_term_probs)))
-            consensus_out.write("{}\n".format(util.prob_to_seq(c_term_probs)))
-
-            prot_out.write(">cluster_{},{},{},{},{}\n".format(cluster_number,
-                                                              allele,
-                                                              num_n,
-                                                              num_c,
-                                                              num_mismatch))
-            prot_out.write("{}\n".format(protein))
-
-            # Write out cluster numbers
-            for bc in unique_bc.keys():
-                # Write cluster/barcode pairs to a file
-                cluster_out.write("{},{},{}\n".format(bc,cluster_number,allele))
-
-            # Flush output files
-            if i % 1000 == 0:
-                cluster_out.flush()
-                consensus_out.flush()
-                prot_out.flush()
-                print(i,"of",len(cluster_files))
-                sys.stdout.flush()
-
-            # HACK HACK HACK
-            if i == 10000:
-                break
+        # Clear any remaining in the batch queue
+        cons, prot, clust = self._run_batch(batch_queue)
+        consensus_out.write("".join(cons))
+        prot_out.write("".join(prot))
+        cluster_out.write("".join(clust))
 
         # Close output files
         cluster_out.close()
